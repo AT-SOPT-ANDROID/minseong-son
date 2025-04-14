3fe3bd6757332a85f8e25da3247e65b5cbcc33ce
sonms <smsms5676@naver.com>
2025-04-14 13:54:39 +0900

    환경 세팅

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,8 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.dagger.hilt)
+    alias(libs.plugins.devtools.ksp)
 }
 
 android {
@@ -41,6 +43,10 @@ android {
 
 dependencies {
     implementation(libs.androidx.datastore.preferences)
+
+    implementation(libs.hilt.navigation.compose)
+    implementation(libs.hilt)
+    ksp(libs.hilt.compiler)
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
