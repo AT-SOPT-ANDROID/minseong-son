cf728ad33b0eb872646c2d03bf449027dc92db59
sonms <smsms5676@naver.com>
2025-04-21 09:56:22 +0900

    feat/히스토리 - 상단 탭과 탭과 연결된 페이저

---
+++ app/src/main/java/org/sopt/at/views/history/pager/HistoryTabPagerScreen.kt
@@ -0,0 +1,104 @@
+package org.sopt.at.views.history.pager
+
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.navigation.NavController
+import org.sopt.at.R
+import org.sopt.at.components.dialogs.DialogType
+import org.sopt.at.views.history.tab.HistoryTabData
+import org.sopt.at.models.history.HistoryEmptyEntity
+import org.sopt.at.viewmodels.HistoryViewModel
+import org.sopt.at.views.history.HistoryLazyGrid
+import org.sopt.at.views.navigation.Route
+
+@Composable
+fun HistoryTabPagerScreen(
+    modifier: Modifier = Modifier,
+    navController: NavController,
+    emptyData : HistoryEmptyEntity?,
+    historyViewModel: HistoryViewModel = hiltViewModel()
+) {
+    val tab by historyViewModel.historyTab.collectAsState()
+    val historyList by historyViewModel.historyDataList.collectAsState(initial = emptyList())
+
+    if (historyList.isEmpty() || tab != HistoryTabData.TAB_SERIES) {
+        Column(
+            modifier = modifier
+                .fillMaxSize()
+                .padding(16.dp)
+        ) {
+            Text(
+                text = emptyData?.title.toString(),
+                fontWeight = FontWeight.Bold,
+                fontSize = 24.sp
+            )
+
+            Text(
+                text = emptyData?.description.toString(),
+                fontSize = 12.sp,
+                color = Color.White,
+                modifier = modifier
+                    .alpha(0.5f)
+            )
+
+            Spacer(modifier = modifier.height(36.dp))
+
+            Text(
+                text = if (emptyData?.id!! % 2 == 0) {
+                    stringResource(id = R.string.text_history_empty_view)
+                } else {
+                    stringResource(id = R.string.text_history_empty_movie)
+                },
+                fontSize = 12.sp,
+                color = Color.White,
+                textAlign = TextAlign.Center,
+                style = MaterialTheme.typography.bodyLarge,
+                modifier = modifier
+                    .border(1.dp, Color.White, RectangleShape)
+                    .padding(16.dp)
+                    .clip(RoundedCornerShape(8.dp))
+                    .align(Alignment.Start)
+                    .clickable {
+                        navController.navigate(Route.HOME) {
+                            popUpTo(Route.HOME)
+                        }
+                    }
+            )
+        }
+    } else {
+        HistoryLazyGrid(
+            modifier = modifier
+                .fillMaxSize(),
+            historyList = historyList
+        ) {
+            if (it != null) {
+                historyViewModel.fetchHistoryData(it)
+                historyViewModel.openHistoryDialog(DialogType.DIALOG_TYPE_DELETE)
+            }
+        }
+    }
+}
---
+++ app/src/main/java/org/sopt/at/views/history/tab/HistoryTabRow.kt
@@ -0,0 +1,77 @@
+package org.sopt.at.views.history.tab
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.pager.PagerState
+import androidx.compose.foundation.pager.rememberPagerState
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.SecondaryTabRow
+import androidx.compose.material3.Tab
+import androidx.compose.material3.TabRowDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import org.sopt.at.ui.theme.ATSOPTANDROIDTheme
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun HistoryTabRow(
+    tabDataList: List<String>,
+    pagerState: PagerState,
+    onTabSelected: (Int) -> Unit,
+    modifier: Modifier = Modifier,
+) {
+    val currentIndex = pagerState.currentPage
+
+    SecondaryTabRow(
+        selectedTabIndex = currentIndex,
+        indicator = {
+            TabRowDefaults.SecondaryIndicator(
+                modifier = modifier.tabIndicatorOffset(currentIndex),
+                color = Color.White
+            )
+        },
+        divider = {} //빈 컴포저블을 넣어 구분선 제거
+    ) {
+        tabDataList.forEachIndexed { index, value ->
+            Tab(selected = currentIndex == index,
+                onClick = {
+                    onTabSelected(index)
+                }
+            ) {
+                Text(
+                    text = value,
+                    color = Color.White,
+                    fontWeight = if (currentIndex == index) {
+                        FontWeight.Bold
+                    } else {
+                        FontWeight.Normal
+                    },
+                    modifier = modifier
+                        .padding(8.dp)
+                )
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun HistoryTabRowPreview() {
+    val pagerState = rememberPagerState (
+        pageCount = { 2 },
+        initialPageOffsetFraction = 0f,
+        initialPage = 0,
+    )
+
+    ATSOPTANDROIDTheme {
+        HistoryTabRow(
+            tabDataList = listOf("시리즈", "영화"),
+            pagerState = pagerState,
+            onTabSelected = {}
+        )
+    }
+}
