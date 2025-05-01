e8982d68e607461c28f7c319634bd6b6c9e04f8a
sonms <smsms5676@naver.com>
2025-05-01 22:38:21 +0900

    Revert "feat/designsystem 모듈 생성으로 관리 - color, typography staticCompositionLocalOf"
    This reverts commit 9ba6917324c399af8c4270012500d392a999781c.

--- designsystem/src/main/java/org/sopt/designsystem/theme/Type.kt
+++
@@ -1,9 +0,0 @@
-package org.sopt.designsystem.theme
-
-import androidx.compose.ui.text.font.Font
-import androidx.compose.ui.text.font.FontFamily
-import com.purang.designsystem.R
-
-val atSoptFontBold = FontFamily(Font(R.font.pretendard_bold))
-val atSoptFontSemiBold = FontFamily(Font(R.font.pretendard_semibold))
-val atSoptFontRegular = FontFamily(Font(R.font.pretendard_regular))
