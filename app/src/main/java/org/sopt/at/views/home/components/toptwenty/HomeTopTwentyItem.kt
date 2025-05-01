a0b6581e0c1bddaaaedadacbf5c8b2e2df55484d
sonms <smsms5676@naver.com>
2025-05-01 22:34:29 +0900

    refactor/smart recomposition - currentRoute, destination

--- app/src/main/java/org/sopt/at/views/home/components/toptwenty/HomeTopTwentyItem.kt
+++ app/src/main/java/org/sopt/at/views/home/components/toptwenty/HomeTopTwentyItem.kt
@@ -10,7 +10,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.platform.LocalWindowInfo
